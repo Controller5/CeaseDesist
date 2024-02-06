f463320ea3aeff510321919cb29f1591fe02efad
Controller5 <128665800+Controller5@users.noreply.github.com>
2024-02-06 09:41:00 -0500

    version 1

--- cease.py
+++ cease.py
@@ -1,42 +1,106 @@
 import streamlit as st
+import pyperclip
+
 
 if "letter" not in st.session_state:
     st.session_state.letter = ""
+if 'platform_tos' not in st.session_state:
+    st.session_state.platform_tos = ""
+if 'platform' not in st.session_state:
+    st.session_state.platform = ""
+if 'st.session_state.platform_tos_args' not in st.session_state:
+    st.session_state.platform_tos_args = ""
+if 'reporting_text' not in st.session_state:
+    st.session_state.reporting_text = ""
+if 'platform_nn' not in st.session_state:
+    st.session_state.platform_nn = ""
+
+
 
 st.title("Cease & Desist Letter")
 
-st.sidebar.header("Guide to finding the right place to send your letter.")
-st.sidebar.markdown(""" Guide here:
-    \n Instagram
-    \n Facebook/Meta
-    \n Google
-    \n 
-    \n X / Twitter
-    \n other site: See Host lookup
-    \n 
- """)
+def sidew(n):
+    st.sidebar.title(n)
+
+
+st.sidebar.title("Guide to finding the right place to send your letter.")
+sidew("Instagram")
+sidew("X/Twitter")
+sidew("Meta/Facebook")
+
+with st.sidebar.container():
+    st.sidebar.write("""_Email:_
+                \nlegal@facebook.com 
+                \n _Send a paper version to:_
+                \n Facebook 
+                \n Attn: Legal Department 
+                \n 156 University Ave 
+                \n Palo Alto, CA \n 94301 """)
+
+sidew("Google")
+sidew("Youtube")
+sidew("Reddit")
 
 
 name = st.text_input("Your name", key="name")
-date_discovery = st.date_input("Date of discovery of doxxing", key="date")
-describe = st.text_area("""Description of the harrassment and sites on which you were doxxed. 
-\n \(Copy/Paste so you don't lose data\)""", key="describe")
-service = st.text_input("Publisher of doxxing materials", key="service")
-today_date = st.date_input("Today's date", key="today")
+service1 = st.selectbox("Platform/Publisher", ['Other', 'Instagram', 'X/Twitter'])
+service2 = st.text_input("Other publisher of doxing materials ",
+                         placeholder="Optional, use only if you marked Other above",
+                         key="service")
 
-if st.button("Generate Letter", key="generate"):
-    st.session_state.letter = f"""
-        Dear {service}
-        \nMy name is {name}. On {date_discovery} I discovered that I had been \"doxxed,\" a recognized form of online harassment. Please find a summary of the details of the harassment herein:
-        \n {describe}
-        \n Signed, 
-        \n /s/ {name} 
-        \n {today_date}
-    """
-    st.download_button("Download Letter as .txt", st.session_state.letter, f"ceasedesist_{name}.txt")
+if service1 == "Other":
+    st.session_state.platform = service2
+if service1 == 'Instagram':
+    st.session_state.platform = 'instagram'
+if service1 == 'X/Twitter':
+    st.session_state.platform = "twitter"
+    st.session_state.platform_nn = "X/Twitter"
+
+
+if st.session_state.platform == 'instagram':
+    st.session_state.platform_tos = "https://www.facebook.com/help/instagram/477434105621119"
+    st.session_state.platform_tos_args = " Instagram's \"Community Guidelines\" in particular the section entitled, \"Follow the law\" and the section entitled, \"Respect other members of the Instagram community,\" states with clarity that Instagram will \"remove content that contains credible threats or hate speech, content that targets private individuals to degrade or shame them, personal information meant to blackmail or harass someone...\""
+    if st.session_state.reported is True:
+        st.session_state.reporting_text = "I reported the material in question via the protocols established in Instagram's Reporting Portal --- Reporting steps / site for Instagram, find."
+if st.session_state.platform == 'twitter':
+    st.session_state.platform_tos = "https://twitter.com/en/tos"
+
+
+url = st.text_input("URL of harassment material", placeholder="URL where the harassment material can be currently found")
+date_discovery = st.date_input("Date of discovery of doxing", key="date", format="MM/DD/YYYY")
+st.checkbox("Have you reported it on the platform/site already?", key="reported")
+describe = st.text_area("""
+Description of the harassment: 
+\n \(Copy/Paste so you don't lose data\)""", key="describe", placeholder="Describe the manner and location of the doxing that you experienced. Describe the harms that you suffered as a result of this harassment.")
+
+today_date = st.date_input("Today's date", key="today", label_visibility="visible", format="MM/DD/YYYY")
+
+with st.container():
+
+    if st.button("Generate Letter", key="generate"):
+        st.session_state.letter = f"""Dear {st.session_state.platform_nn},
+            \nMy name is {name}. On {date_discovery} I discovered that I had been \"doxed,\" a recognized form of online harassment. Please find a summary of the details of the harassment herein:
+            \n{describe}
+            \nPublication of the doxing materials are to be found at {url} as of {date_discovery}.  {st.session_state.reporting_text}
+            \n Doxing is an English word, short for Dropping Documents, for harassment through the publishing of PII (Personally Identifying Information). Because of the interconnected nature of the internet, PII published online spreads quickly around the world and can cause irreparable harm to a doxed individual.
+            \n This letter shall serve as notice that absent the swift removal from {st.session_state.platform_nn}'s site of the aforemention doxing material I am prepared to retain an attorney and take steps towards the resolution of this matter. The harassment published on your platform may violate federal law against stalking, and/or state law, including but not limited to, in the states of Arizona, Colorado, Florida, Kentucky, Minnesota, Oklahoma, and Oregon, each of whom have laws explicitly targeting the practice of doxing. 
+            \nFurther, I am prepared to retain counsel and seek resolution under the laws of various foreign nations in which this published material is accessible, including but not limited to, France, Germany, the United Kingdom, and Canada. 
+            \nThe harassment materials published at {url} fall strictly within the language regarding prohibited postings found at {st.session_state.platform_nn}'s Terms of Service found at {st.session_state.platform_tos} {st.session_state.platform_tos_args}
+            \nFinally, I appeal to the humanity within your company to recognize that this harassment has been harmful to me and should end immediately. Thank you for your time in consideration of this letter.
+            \nYours sincerely,
+            \n/s/ {name}
+            \n{today_date}
+        """
+        st.success("Letter Generated")
+
+    if st.button("Copy to Clipboard", key="copy"):
+        pyperclip.copy(st.session_state.letter)
+        st.success("Copied to Clipboard")
+
+    if st.download_button("Download Letter",
+        st.session_state.letter,
+        f"ceasedesist_{name}.txt"):
+            st.success("Download Initiated")
+
 st.divider()
-st.markdown(st.session_state.letter)
-
-
-
-
+st.write(st.session_state.letter)
---
+++ notes.md
@@ -0,0 +1,20 @@
+# Notes 
+
+Include language on
+
+- Federal Stalking - internet interstate
+- State Laws
+  - As of the end of 2021, seven states-Arizona, Colorado, Florida,
+  Kentucky, Minnesota, Oklahoma, and Oregon-had enacted laws explicitly
+  targeting the practice of "doxing,"" (Thinking outside the dox)
+  - "prepared to seek legal representation in any one of those states."
+  - 
+
+
+- Mention CDA, but say this goes beyond, 
+  - Puts them on notice.
+- Terms of Use 
+  - Can make particular to each platform cite to Terms of use
+  - Drop down + other
+  - if st.ss.platform == "Instagram"
+    - st.ss.tou = "Instagram terms of use paragraph"
