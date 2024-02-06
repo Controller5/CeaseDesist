e321ecf63926916199d1edbcaf01391f8be87e9a
Controller5 <128665800+Controller5@users.noreply.github.com>
2024-02-06 10:21:39 -0500

    language change

--- cease.py
+++ cease.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit import session_state as ss
 import pyperclip
 
+st.set_page_config(page_title="Cease and Desist", page_icon=":stop:")
 
 if "letter" not in st.session_state:
     st.session_state.letter = ""
